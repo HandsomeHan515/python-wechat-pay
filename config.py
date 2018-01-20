9c65685e4b858e04034181b985a14fa68b3246d8
HandsomeHan <HandsomeHan515@163.com>
2018-01-20 14:53:25 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+APP_ID = ''
+
+APP_SECRET = ''
+
+MCH_ID = ''
+
+SIGN_KEY = ''
+
+UNIFIED_ORDER_URL': 'https://api.mch.weixin.qq.com/pay/unifiedorder',
+
+ORDER_QUERY_URL': 'https://api.mch.weixin.qq.com/pay/orderquery',
